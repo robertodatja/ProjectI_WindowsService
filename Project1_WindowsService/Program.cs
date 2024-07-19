01fd093732add6de18ec6296c5bbf37ffb3c0785
Roberto Datja <72038709+robertodatja@users.noreply.github.com>
2024-07-19 11:32:38 +0200

    Add files via upload

---
+++ Project1_WindowsService/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+using Project1_WindowsService;
+
+var configFile = new ConfigurationBuilder().AddJsonFile("appsettings.json", false, true).Build();
+
+var services = new ServiceCollection();
+services
+    .AddLogging(log => { log.ClearProviders(); log.AddNLog(); }) //i krijon vete logFactory logProvider
+    .AddSingleton(configFile)
+    .AddScoped<TestLogin>()
+    ;
+
+//BuildServiceProvider() e krijon Servisin per injektim
+using (var serviceProvider = services.BuildServiceProvider())
+{
+    var objekti = serviceProvider.GetRequiredService<TestLogin>();
+    objekti.Shenim();
+}
---
+++ Project1_WindowsService/TestLogin.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1_WindowsService
+{
+    //Kur te krijohet nje objekt TestLogin, le te injektohet nje logger,
+    //e cila i jepet si vlere variablit _logger
+    public class TestLogin
+    {
+        private readonly ILogger<TestLogin> _logger; //1  login pra nlog.config
+        private readonly IConfigurationRoot _config; //2 Configuration File pra appsettings.json
+        public TestLogin(ILogger<TestLogin> logger, IConfigurationRoot config) //ctor
+        {
+            _logger = logger; 
+            _config = config; 
+        }
+
+        public void Shenim() //3 me shenuar ne login
+        {
+            var test = _config["Folderi"]; //i qasemi si Dictionary
+            _logger.LogInformation($"Shenuam dicka ne log {test}");
+            _logger.LogError("Gabim Gabim");
+        }
+    }
+}
